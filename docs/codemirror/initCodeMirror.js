1cf9a658a559a6f33fa70eeae6d31c074204f89f
Thomas Mueller <thomasm@apache.org>
2024-07-06 14:07:24 +0200

    Mobile

--- docs/codemirror/initCodeMirror.js
+++ docs/codemirror/initCodeMirror.js
@@ -2,6 +2,5 @@ this.inputEditor = CodeMirror.fromTextArea(document.getElementById("source"), {
     lineNumbers: false, mode: "text/bau"
 });
 setInterval(function () {
-    inputEditor.refresh();
     inputEditor.save();
 }, 200)
