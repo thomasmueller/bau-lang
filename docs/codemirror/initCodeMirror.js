7b4ef427488ff1e6cfe15d4eab08b2e6cf8fcdf9
Thomas Mueller <thomasm@apache.org>
2024-07-07 14:15:57 +0200

    Playground

--- docs/codemirror/initCodeMirror.js
+++ docs/codemirror/initCodeMirror.js
@@ -1,6 +1,9 @@
-this.inputEditor = CodeMirror.fromTextArea(document.getElementById("source"), {
-    lineNumbers: false, mode: "text/bau"
-});
-setInterval(function () {
-    inputEditor.save();
-}, 200)
+var width = (window.innerWidth > 0) ? window.innerWidth : document.documentElement.clientWidth;
+if (width > 900) {
+    this.inputEditor = CodeMirror.fromTextArea(document.getElementById("source"), {
+        lineNumbers: false, mode: "text/bau"
+    });
+    setInterval(function () {
+        inputEditor.save();
+    }, 200)
+}
