1bd5023bc56ba1a482275f35119cf3ef9562a0eb
Thomas Mueller <mueller@adobe.com>
2025-12-01 18:16:34 +0100

    String utilities: improved compatibility with Unicode (root locale)

--- src/test/java/org/bau/stdlib/string/StringUtils.java
+++ src/test/java/org/bau/stdlib/string/StringUtils.java
@@ -4,6 +4,28 @@ import java.util.ArrayList;
 
 public class StringUtils {
 
+    public static String trim(String s) {
+        return s.trim();
+    }
+
+    public static String trimLeading(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) > ' ') {
+                return s.substring(i);
+            }
+        }
+        return "";
+    }
+
+    public static String trimTrailing(String s) {
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) > ' ') {
+                return s.substring(0, i + 1);
+            }
+        }
+        return "";
+    }
+
     /**
      * Replace "before" with "after". If "before" is empty, then the original string
      * is returned.
@@ -56,6 +78,170 @@ public class StringUtils {
         }
         list.add(s.substring(index));
         return list.toArray(new String[list.size()]);
+    }
+
+    public static boolean isSpace(char x) {
+        // tab, newline, vertical tab, formfeed, return, space
+        long mask = (1L << 0x9) | (1L << 0xa) | (1L << 0xb) | (1L << 0xc) | (1L << 0xd) | (1L << 0x20);
+        return x <= ' ' && ((1L << x) & mask) != 0;
+    }
+
+    public static boolean isDigit(char x) {
+        return x >= '0' && x <= '9';
+    }
+
+    public static char charAt(String s, int index) {
+        if (index < s.length()) {
+            return s.charAt(index);
+        }
+        return 0;
+    }
+
+    public static String toUpper(String s) {
+        int i = 0;
+        for (; i < s.length(); i++) {
+            char x = s.charAt(i);
+            char y = toUpper(x);
+            if (x != y) {
+                char[] chars = s.toCharArray();
+                while (true) {
+                    chars[i] = y;
+                    i++;
+                    if (i >= chars.length) {
+                        break;
+                    }
+                    y = toUpper(chars[i]);
+                }
+                return new String(chars);
+            }
+        }
+        return s;
+    }
+
+    public static String toLower(String s) {
+        int i = 0;
+        for (; i < s.length(); i++) {
+            char x = s.charAt(i);
+            char y = toLower(x);
+            if (x != y) {
+                char[] chars = s.toCharArray();
+                while (true) {
+                    chars[i] = y;
+                    i++;
+                    if (i >= chars.length) {
+                        break;
+                    }
+                    y = toLower(chars[i]);
+                }
+                return new String(chars);
+            }
+        }
+        return s;
+    }
+
+    public static char toUpper(char x) {
+        if (x >= 'a' && x <= 'z') {
+            return (char) (x - 'a' + 'A');
+        }
+        return x;
+    }
+
+    public static char toLower(char x) {
+        if (x >= 'A' && x <= 'Z') {
+            return (char) (x - 'A' + 'a');
+        }
+        return x;
+    }
+
+    public static int naturalCompare(String x, String y) {
+        return naturalCompare(x, y, false);
+    }
+
+    public static int compareCaseInsensitive(String x, String y) {
+        int xi = 0, yi = 0;
+        while (xi < x.length() && yi < y.length()) {
+            char xc = toUpper(x.charAt(xi));
+            char yc = toUpper(y.charAt(yi));
+            if (xc > yc) {
+                return 1;
+            } else if (xc < yc) {
+                return -1;
+            }
+            xi++;
+            yi++;
+        }
+        return Integer.compare(x.length(), y.length());
+    }
+
+    public static int naturalCompare(String x, String y, boolean caseInsensitive) {
+        int xi = 0, yi = 0;
+        while (true) {
+            char xc = charAt(x, xi);
+            while (isSpace(xc)) {
+                xi++;
+                xc = charAt(x, xi);
+            }
+            char yc = charAt(y, yi);
+            while (isSpace(yc)) {
+                yi++;
+                yc = charAt(y, yi);
+            }
+            if (isDigit(xc) && isDigit(yc)) {
+                boolean left = xc == '0' || yc == '0';
+                int result = 0;
+                while(true) {
+                    xc = charAt(x, xi);
+                    yc = charAt(y, yi);
+                    if (!isDigit(xc) && !isDigit(yc)) {
+                        if (result != 0) {
+                            return result;
+                        }
+                        break;
+                    } else if (!isDigit(xc)) {
+                        return -1;
+                    } else if (!isDigit(yc)) {
+                        return 1;
+                    } else if (xc < yc && result == 0) {
+                        result = -1;
+                    } else if (xc > yc && result == 0) {
+                        result = 1;
+                    }
+                    if (left && result != 0) {
+                        return result;
+                    }
+                    xi++;
+                    yi++;
+                }
+            }
+            if (xc == 0 && yc == 0) {
+                return 0;
+            }
+            if (caseInsensitive) {
+                xc = toUpper(xc);
+                yc = toUpper(yc);
+            }
+            if (xc > yc) {
+                return 1;
+            } else if (xc < yc) {
+                return -1;
+            }
+            xi++;
+            yi++;
+        }
+    }
+
+    public static char toUpperChar(char c) {
+        if ((c >= 'a' && c <= 'z') || (c >= 224 && c <= 254 && c != 247)) {
+            return (char) (c ^ 32);
+        }
+        return c;
+    }
+
+    public static char toLowerChar(char c) {
+        if ((c >= 'A' && c <= 'Z') || (c >= 192 && c <= 222 && c != 215)) {
+            return (char) (c ^ 32);
+        }
+        return c;
     }
 
 }
---
+++ src/test/java/org/bau/stdlib/string/StringUtilsTest.java
@@ -0,0 +1,177 @@
+package org.bau.stdlib.string;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Random;
+
+import org.junit.Test;
+
+public class StringUtilsTest {
+
+    // ISO-8859-1 range (Basic Latin and Latin-1 Supplement)
+    // lower, upper, collaction strength 0, 1, 2, 3
+
+    // TODO comparison with different strengths
+
+    // TODO for unicode code points 0..255, support upper, lower, comparison strength 1, 2, 3; types?
+
+    // https://en.wikipedia.org/wiki/Unicode_collation_algorithm
+    // https://github.com/railgunlabs/unicorn
+
+
+    @Test
+    public void isX() {
+        for (int i = 0; i < 256; i++) {
+            if ((" " + (char) 9 + (char) 10 + (char) 11 + (char) 12 + (char) 13).indexOf(i) >= 0) {
+                assertTrue(StringUtils.isSpace((char) i));
+            } else {
+                assertFalse("" + i, StringUtils.isSpace((char) i));
+            }
+            if (i >= '0' && i <= '9') {
+                assertTrue(StringUtils.isDigit((char) i));
+            } else {
+                assertFalse(StringUtils.isDigit((char) i));
+            }
+        }
+    }
+
+    @Test
+    public void upper() {
+        for (int i = 0; i < 256; i++) {
+            String t = "" + (char) i;
+            char[] upperChars = t.toUpperCase(Locale.ROOT).toCharArray();
+            int upper;
+            if (upperChars.length > 1 || upperChars[0] > 0xff) {
+                upper = i;
+            } else {
+                upper = upperChars[0];
+            }
+            assertEquals("char " + i, upper, (int) StringUtils.toUpperChar((char) i));
+        }
+    }
+
+    @Test
+    public void lower() {
+        for (int i = 0; i < 256; i++) {
+            String t = "" + (char) i;
+            char[] lowerChars = t.toLowerCase(Locale.ROOT).toCharArray();
+            int lower;
+            if (lowerChars.length > 1 || lowerChars[0] > 0xff) {
+                lower = i;
+            } else {
+                lower = lowerChars[0];
+            }
+            assertEquals("char " + i, lower, (int) StringUtils.toLowerChar((char) i));
+        }
+    }
+
+    @Test
+    public void trim() {
+        assertEquals("test", StringUtils.trim(" test "));
+        assertEquals("test ", StringUtils.trimLeading(" test "));
+        assertEquals(" test", StringUtils.trimTrailing(" test "));
+        assertEquals("", StringUtils.trim(" "));
+        assertEquals("", StringUtils.trimLeading(" "));
+        assertEquals("", StringUtils.trimTrailing(" "));
+    }
+
+    @Test
+    public void toUpperLower() {
+        assertEquals("HELLO", StringUtils.toUpper("Hello"));
+        assertEquals("HELLO", StringUtils.toUpper("HELLO"));
+        assertEquals("HELLO WORLD", StringUtils.toUpper("hello World"));
+        assertEquals("hello", StringUtils.toLower("Hello"));
+        assertEquals("hello", StringUtils.toLower("hello"));
+        assertEquals("hello world", StringUtils.toLower("hello World"));
+    }
+
+    @Test
+    public void compareCaseInsensitive() {
+        assertEquals(0, StringUtils.compareCaseInsensitive("hello", "HELLO"));
+        assertEquals(-1, StringUtils.compareCaseInsensitive("hello", "hello world"));
+        assertEquals(1, StringUtils.compareCaseInsensitive("hello world", "hello"));
+        assertEquals(-1, StringUtils.compareCaseInsensitive("hello", "world"));
+        assertEquals(-1, StringUtils.compareCaseInsensitive("hello 1", "hello 2"));
+        assertEquals(1, StringUtils.compareCaseInsensitive("hello 2", "hello 1"));
+    }
+
+    @Test
+    public void naturalSort() {
+        // https://github.com/sourcefrog/natsort.git
+        String[] list = new String[] { "1.001", "1.002", "1.010", "1.02", "1.1", "1.3",
+                "a", "a0", "a1", "a1a", "a1b", "a2", "a10", "a20",
+                "a space 1", "a    space  2", "a space      3",
+                "x2-g8", "x2-y08", "x2-y7", "x8-y8",
+                "za-0!", "za-0a", "za-01", "za-01z", "zb-20",
+                "zb-101", "zb-102", "zb-201", "zc 10", "zc11", "zc 12" };
+        for (int i = 0; i < list.length; i++) {
+            if (i > 0) {
+                String prev = list[i - 1];
+                String current = list[i];
+                assertEquals(prev + "<" + current, -1, StringUtils.naturalCompare(prev, current));
+                assertEquals(current + ">" + prev, 1, StringUtils.naturalCompare(current, prev));
+                assertEquals(0, StringUtils.naturalCompare(current, current));
+            }
+        }
+        Random r = new Random(1);
+        for (int i = 0; i < 1000; i++) {
+            String x = randomString(r, "  0123ab!AB.~");
+            String y = randomString(r, "  0123ab!AB.~");
+            StringUtils.naturalCompare(x, y);
+            StringUtils.naturalCompare(y, x);
+            StringUtils.naturalCompare(x, x);
+            StringUtils.naturalCompare(x, y, true);
+            StringUtils.naturalCompare(y, x, true);
+            StringUtils.naturalCompare(x, x, true);
+        }
+    }
+
+    private String randomString(Random r, String string) {
+        int len = r.nextInt(10);
+        StringBuilder buff = new StringBuilder(len);
+        for (int i = 0; i < len; i++) {
+            buff.append(string.charAt(r.nextInt(string.length())));
+        }
+        return buff.toString();
+    }
+
+    @Test
+    public void split() {
+        assertEquals("[abc]", Arrays.toString(StringUtils.split("abc", 'x')));
+        assertEquals("[a, b, c]", Arrays.toString(StringUtils.split("a|b|c", '|')));
+        assertEquals("[, b, , ]", Arrays.toString(StringUtils.split("|b||", '|')));
+        assertEquals("[, b, , ]", Arrays.toString(";b;;".split(";", -1)));
+    }
+
+    @Test
+    public void replaceAll() {
+        assertEquals("", StringUtils.replaceAll("", "", ""));
+        assertEquals("abc", StringUtils.replaceAll("abc", "", ""));
+        assertEquals("abc", StringUtils.replaceAll("abc", "", "x"));
+        assertEquals("aabc", StringUtils.replaceAll("abc", "a", "aa"));
+        assertEquals("ba", StringUtils.replaceAll("aaa", "aa", "b"));
+        Random r = new Random(1);
+        for (int i = 0; i < 1000; i++) {
+            StringBuilder original = new StringBuilder();
+            StringBuilder replace = new StringBuilder();
+            StringBuilder with = new StringBuilder();
+            for (int j = r.nextInt(10); j >= 0; j--) {
+                original.append("abc".charAt(r.nextInt(3)));
+            }
+            for (int j = r.nextInt(4); j >= 0; j--) {
+                replace.append("abc".charAt(r.nextInt(3)));
+            }
+            for (int j = r.nextInt(4); j >= 0; j--) {
+                replace.append("abc".charAt(r.nextInt(3)));
+            }
+            String expected = original.toString().replace(replace.toString(), with.toString());
+            String got = StringUtils.replaceAll(original.toString(), replace.toString(), with.toString());
+            assertEquals(expected, got);
+        }
+    }
+
+}
